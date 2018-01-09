423fccf8e5f68670a813f96da081628128210b14
EucliTs0 <dimitri_ts0@hotmail.com>
2018-01-10 00:02:47 +0200

    Update build_hist.m

--- build_hist.m
+++ build_hist.m
@@ -1,12 +1,17 @@
 function H = build_hist(I, forest, centers, opts)
 
+%Dense SIFT features
 [~, d] = vl_dsift(I, opts{:});
+
+%L2 normalize the SIFt features
 d = normalize_features(d);
    
 [index , ~] = vl_kdtreequery(forest , centers , d);
 
+ %Compute the image histogram 
  H = vl_binsum(zeros(length(centers),1), 1, double(index)) ;
  
+ %L1 normalize the histogram
  H = H ./ sum(H);
  
  H = H';
