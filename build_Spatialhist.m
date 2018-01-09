03a6991c7e543dbc91519f4fe1260c015ec5f031
EucliTs0 <dimitri_ts0@hotmail.com>
2018-01-10 00:01:16 +0200

    Update build_Spatialhist.m

--- build_Spatialhist.m
+++ build_Spatialhist.m
@@ -1,21 +1,27 @@
 function H = build_Spatialhist(I, forest, centers, opts)
 
+%spatialX and spatialX defines the image region. For example spatialX = [1 1] and spatialY = [1 3]
+%means that we compute the histogram on the whole image (spatialX = 1, spatialY = 1) and also on the three vertical
+%slices (spatialX = 1, spatialY = 3).
+
 spatialX = [1 1];
 spatialY = [1 3];
 width = size(I, 2);
 height = size(I, 1);
 [frames, d] = vl_dsift(I, opts{:});
+
+%L2 normalize the features
 d = normalize_features(d);
 
    
 [index , ~] = vl_kdtreequery(forest , centers , d);
 %[drop, index] = min(vl_alldist(centers, single(d)), [], 1) ;
 for i = 1:length(spatialX)
-    
+    %Compute histograms on all regions
     binsx = vl_binsearch(linspace(1,width,spatialX(i)+1), frames(1,:)) ;
     binsy = vl_binsearch(linspace(1,height,spatialY(i)+1), frames(2,:)) ;
 
-  % combined quantization
+  % combine the histograms
     bins = sub2ind([spatialY(i), spatialX(i), length(centers)], ...
                  binsy,binsx,index) ;
     hist = zeros(spatialY(i) * spatialX(i) * length(centers), 1) ;
