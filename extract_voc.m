5b294a4951e77e08426dc333f079191771df5752
EucliTs0 <dimitri_ts0@hotmail.com>
2018-01-10 00:10:03 +0200

    Update extract_voc.m

--- extract_voc.m
+++ extract_voc.m
@@ -1,15 +1,12 @@
 function features = extract_voc(I, opts)
 
-magnif = 3;
-binSize = 8;
-I = vl_imsmooth(im2single(I), sqrt((binSize/magnif)^2 - .25));
 [~, features] = vl_dsift(I, opts{:});
-features = snorm(features);
+features = normalize_features(features);
 
 end
 
-function x = snorm(x)
-x = bsxfun(@times, x, 1./max(1e-5,sqrt(sum(x.^2,1)))) ;
+function x = normalize_features(x)
+x = bsxfun(@rdivide, x, sqrt(sum(x.^2,1))) ;
 end
 
 
